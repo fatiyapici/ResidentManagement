28ea8cbd485aadad0fe9d85eb89d9f93a76b3df8
Fatih Yapıcı <fa.yapici@gmail.com>
2023-10-09 16:20:28 +0300

    initialize commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ResidentManagement.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
+
