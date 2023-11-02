417f2219bada54ed445b682a6a96d6cd4705cb91
Fatih Yapıcı <fa.yapici@gmail.com>
2023-11-02 22:40:43 +0300

    fix apartment user id map

--- Models/Apartment.cs
+++ Models/Apartment.cs
@@ -3,7 +3,7 @@ namespace ResidentManagement;
 public class Apartment
 {
     public int ID { get; set; }
-    public int UserID { get; set; }
+    public string UserId { get; set; }
     public int Number { get; set; }
     public int Floor { get; set; }
     public string Block { get; set; }
