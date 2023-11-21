c5394692b7e7e0e1351547a7dfba85ec0aea434c
Fatih Yapıcı <fa.yapici@gmail.com>
2023-11-21 14:15:07 +0300

    add bill view model

---
+++ ViewModels/BillCreateViewModel.cs
@@ -0,0 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ResidentManagement;
+
+public class BillCreateViewModel
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+
+    [DataType(DataType.Date)]
+    [DisplayFormat(DataFormatString = "{0:MM-yyyy}", ApplyFormatInEditMode = true)]
+    public string Session { get; set; }
+
+    [Column(TypeName = "decimal(18, 2)")]
+    public decimal Amount { get; set; }
+    public string? Description { get; set; }
+}
