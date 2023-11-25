0a6eceaa670b5e819f098304e4c5f0ed594fb7e0
Fatih Yapıcı <fa.yapici@gmail.com>
2023-11-25 21:42:28 +0300

    fix invoice edit get and post

--- Controllers/InvoiceController.cs
+++ Controllers/InvoiceController.cs
@@ -122,20 +122,38 @@ namespace ResidentManagement.Controllers
         // GET: Invoice/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
             if (id == null || _context.Invoices == null)
             {
                 return NotFound();
             }
 
             var invoice = await _context.Invoices.FindAsync(id);
+
             if (invoice == null)
             {
-                return NotFound();
+                return NotFound("Invoice not found.");
             }
-            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "ID", "ID", invoice.ApartmentId);
-            var viewModel = new InvoiceViewModel();
-            viewModel.Session = DateTime.Now.ToString("yyyy-MM");
-            return View(invoice);
+            var invoiceViewModel = new InvoiceViewModel();
+
+            invoiceViewModel.ApartmentId = invoice.ApartmentId;
+            invoiceViewModel.Session = invoice.Session.ToString("yyyy-MM");
+            invoiceViewModel.Amount = invoice.Amount;
+            invoiceViewModel.Description = invoice.Description;
+
+            var userApartments = await _context.Apartments
+                .Where(x => x.UserId == user.Id)
+                .Select(x => new ApartmentDropdownOptions(x.ID, "Number: " + x.Number + " Floor: " + x.Floor + " Block: " + x.Block))
+                .ToListAsync();
+
+            ViewData["UserApartments"] = new SelectList(userApartments, "ID", "NumberFloorBlockInfo", invoiceViewModel.ApartmentId);
+            invoiceViewModel.Session = DateTime.Now.ToString("yyyy-MM");
+            return View(invoiceViewModel);
         }
 
         // POST: Invoice/Edit/5
@@ -143,35 +161,63 @@ namespace ResidentManagement.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("ID,ApartmentId,Session,Amount,Description")] Invoice invoice)
+        public async Task<IActionResult> Edit(int id, [Bind("ID,ApartmentId,Session,Amount,Description")] InvoiceViewModel invoiceViewModel)
         {
-            if (id != invoice.ID)
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound("User not found.");
+            }
+
+            if (id != invoiceViewModel.ID)
             {
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            if (!ModelState.IsValid)
             {
-                try
+                var invoiceApartment = await _context.Apartments
+                .Where(x => x.UserId == user.Id)
+                .Select(x => new ApartmentDropdownOptions(x.ID, "Number: " + x.Number + " Floor: " + x.Floor + " Block: " + x.Block))
+                .ToListAsync();
+
+                ViewData["UserApartments"] = new SelectList(invoiceApartment, "ID", "NumberFloorBlockInfo", invoiceViewModel.ApartmentId);
+                return View(invoiceViewModel);
+            }
+
+            try
+            {
+                var invoice = await _context.Invoices.FirstOrDefaultAsync(x => x.ID == invoiceViewModel.ID);
+                if (invoice == null)
                 {
+                    return NotFound("Invoice not found.");
+                }
+
+                DateTime sessionDate;
+                if (DateTime.TryParse(invoiceViewModel.Session, out sessionDate))
+                {
+                    invoice.ApartmentId = invoiceViewModel.ApartmentId;
+                    invoice.Session = sessionDate;
+                    invoice.Amount = invoiceViewModel.Amount;
+                    invoice.Description = invoiceViewModel.Description;
+
                     _context.Update(invoice);
                     await _context.SaveChangesAsync();
                 }
-                catch (DbUpdateConcurrencyException)
-                {
-                    if (!InvoiceExists(invoice.ID))
-                    {
-                        return NotFound();
-                    }
-                    else
-                    {
-                        throw;
-                    }
-                }
-                return RedirectToAction(nameof(Index));
             }
-            ViewData["ApartmentId"] = new SelectList(_context.Apartments, "ID", "ID", invoice.ApartmentId);
-            return View(invoice);
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!InvoiceExists(invoiceViewModel.ID))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToAction(nameof(Index));
         }
 
         // GET: Invoice/Delete/5
@@ -202,6 +248,7 @@ namespace ResidentManagement.Controllers
             {
                 return Problem("Entity set 'ApplicationDbContext.Invoices'  is null.");
             }
+
             var invoice = await _context.Invoices.FindAsync(id);
             if (invoice != null)
             {
